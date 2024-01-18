7298e8c6e8e86a445770f4a640f5a42261cd6bc2
matheusmntt <matheus.monteiro7@outlook.com>
2024-01-18 00:15:05 -0300

    :tada: Commit inicial

---
+++ src/screens/Groups/index.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from 'react-native';
+import { s } from './styles';
+
+export const Groups = () => {
+  return (
+    <View style={s.container}>
+      <Text>funcionou!</Text>
+    </View>
+  );
+}
+
+
