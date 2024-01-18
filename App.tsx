7141590501d373b2effeafe39e165c005399800c
matheusmntt <matheus.monteiro7@outlook.com>
2024-01-18 01:50:16 -0300

    feat: import font roboto, theme provider, stylized status bar, import loading component

--- App.tsx
+++ App.tsx
@@ -1,8 +1,23 @@
-import { Groups } from "@screens/Groups";
+import { Groups } from '@screens/Groups'
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
+import { ThemeProvider } from 'styled-components'  
+import { Loading } from '@components/Loading'
+import theme from '@theme/index'
+import { StatusBar } from 'react-native'
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
+  
   return (
-    <Groups />
+    <ThemeProvider theme={theme}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      {fontsLoaded && <Groups />}
+      {!fontsLoaded && <Loading />}
+    </ThemeProvider>
   )
 }
  
