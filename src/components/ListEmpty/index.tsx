9f0ef24078562f66d95abc3cff42880d862001a4
matheusmntt <matheus.monteiro7@outlook.com>
2024-01-18 23:39:49 -0300

    feat: list empty component

---
+++ src/components/ListEmpty/index.tsx
@@ -0,0 +1,15 @@
+import { Container, Message } from "./styles"
+
+type ListEmptyProps = {
+  message: string
+}
+
+export const ListEmpty = ({ message }: ListEmptyProps) => {
+  return (
+    <Container>
+      <Message>
+        {message}
+      </Message>
+    </Container>
+  )
+}
