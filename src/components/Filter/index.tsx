91ba4442e3cb82b52739c69f072ad4063a82e7d6
matheusmntt <matheus.monteiro7@outlook.com>
2024-01-18 23:39:07 -0300

    feat: filter component

---
+++ src/components/Filter/index.tsx
@@ -0,0 +1,17 @@
+import { TouchableOpacityProps } from 'react-native'
+
+import { Container, FilterStyleProps, Title } from "./styles"
+
+type FilterProps = TouchableOpacityProps & FilterStyleProps & {
+  title: string
+}
+
+export const Filter = ({ title, isActive = false, ...rest }: FilterProps) => {
+  return (
+    <Container isActive={isActive} {...rest}>
+      <Title>
+        {title}
+      </Title>
+    </Container>
+  )
+}
